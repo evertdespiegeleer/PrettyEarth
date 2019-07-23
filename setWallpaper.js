cb9edc55d2731e920ed3f607d9c35d13f35582a3
akramsaouri <ak.saouri@gmail.com>
2019-07-23 20:29:15 +0100

    [UPD] setWallpaper command to allow multiple screens

--- setWallpaper.js
+++ setWallpaper.js
@@ -3,7 +3,7 @@ import path from 'path'
 const exec = util.promisify(require('child_process').exec)
 const setWallpaper = (downloadloc, cb) => {
   const osvar = process.platform
-  const command = `osascript -e 'tell application "Finder" to set desktop picture to POSIX file "${path.resolve(downloadloc)}"'`
+  const command = `osascript -e 'tell application "System Events" to tell every desktop to set picture to "${path.resolve(downloadloc)}"'`
   if (osvar === 'darwin') { // currently only macOS is supported.
     exec(command, (err, stdout, stderr) => {
       if (err) {
